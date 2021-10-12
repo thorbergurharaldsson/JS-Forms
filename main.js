5db770b012cd1dba8861a8ddaa1d9973789865f3
thorbergurharaldsson <thorbergur@haraldssonprod.is>
2021-10-12 15:07:17 +0000

    Added formValidation to validate the form as a whole and route to success.html if the form is valid

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let id = (id) => document.getElementById(id);
 
 let classes = (classes) => document.getElementsByClassName(classes);
 
+let formValidation = false;
+
 let username = id("username"),
   email = id("email"),
   job = id("job"),
@@ -14,19 +16,23 @@ let username = id("username"),
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
+  formValidation = true;
   engine(username, 0, "Username cannot be blank");
   engine(email, 1, "Email cannot be blank");
   engine(job, 2, "Jobtitle cannot be blank");
   engine(date, 3, "Birthdate cannot be blank");
   engine(phone, 4, "Phone number cannot be blank");
+
+  if (formValidation) {
+    document.location.href = "success.html";
+  }
 });
 
 let engine = (id, serial, message) => {
   // If the field is empty
   if (id.value.trim() === "") {
     errorMsg[serial].innerHTML = message; // Display error message
-
+    formValidation = false;
     id.style.border = "2px solid red"; // Make boarder red to indicate an unsuccessful validation
 
     // icons
